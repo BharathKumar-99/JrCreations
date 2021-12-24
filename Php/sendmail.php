05b3e71b33b8e59a713a4093722c4ef6bf61f06f
bharath <bharathkumar6362@gmail.com>
2021-12-24 12:37:16 +0530

    mail system update

--- Php/sendmail.php
+++ Php/sendmail.php
@@ -7,14 +7,20 @@
    <body>
       
       <?php
+      $name = $_POST['name'];
+      $email = $_POST['email'];
+      $phone = $_POST['tel'];
+      $mes =$_POST['msg'];
+      $sub=$_POST['sub'];
          $to = "bharthblaster@gmail.com";
-         $subject = "This is subject";
          
-         $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
+         $subject = $sub;
          
-         $header = "From:abc@somedomain.com \r\n";
-         $header .= "Cc:afgh@somedomain.com \r\n";
+         $message = "$name <br> $email <br> $phone <br> $mes <br> " ;
+      
+         
+         $header = "From:Orders@jr.com \r\n";
+        
          $header .= "MIME-Version: 1.0\r\n";
          $header .= "Content-type: text/html\r\n";
          
